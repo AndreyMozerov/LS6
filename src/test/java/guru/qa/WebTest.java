e93f32f373b6f59ef3813aacbe74d41f36eabc76
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-12-04 17:58:51 +0300

    v9

--- src/test/java/guru/qa/WebTest.java
+++ src/test/java/guru/qa/WebTest.java
@@ -1,20 +1,37 @@
 package guru.qa;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WebTest {
 
+    @BeforeEach
+    void setup() {
+        open("https://google.com");
+    }
+
     @DisplayName("Проверка наличия url 'https://selenide.org'" +
-            "в результатах выдачи гугла по запросу 'selenide' ")
-      @Test
-    void googleTest(){
-          open("https://google.com");
+            "в результатах выдачи гугла по запросу 'selenide'")
+    @Test
+    @Tag("BLOCKER")
+    void googleTest() {
+
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+
+    }
+    @Disabled("1")
+    @DisplayName("Проверка  popup загрузки фото")
+    @Test
+    //@Tag("BLOCKER")
+    void googlePhotoTest(){
+
+        $("img[alt='Поиск с помощью камеры']").click();
+        $(byText("Выполните поиск по изображению в Google Объективе")).shouldBe(visible );
       }
 }
