f1fa7e57898139adfabc94da68da765478259586
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-12-09 13:22:42 +0300

    v3

---
+++ src/test/java/Bramtech/BramCSVTest.java
@@ -0,0 +1,34 @@
+package Bramtech;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BramCSVTest {
+
+    @BeforeEach
+    void setUp() {
+        open("https://google.com");
+    }
+
+
+
+    @CsvSource(value = {
+            "selenide, https://selenide.org, 1",
+            "junit 5, https://junit.org, 1"
+    })
+    @ParameterizedTest(name = "Проверка наличия урла {1}" +
+            "в результатах выдачи гугла по запросу {0}")
+    @Tags({@Tag("BLOCKER"), @Tag("FEATURE")})
+    void googleSearchTest(String searchQuery, String expectedUrl, int resultCount) {
+        $("[name=q]").setValue(searchQuery).pressEnter();
+        $("[id=search]").shouldHave(text(expectedUrl));
+        $$("[id=search]").should(CollectionCondition.size(resultCount));
+    }
+}
